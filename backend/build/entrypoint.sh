be6beda7ecc83c21ee83ef7588f02adf2e024dc6
Patrick <84373546+Z3PP1X@users.noreply.github.com>
2025-11-25 15:57:18 +0100

    Frontend running

--- backend/build/entrypoint.sh
+++ backend/build/entrypoint.sh
@@ -4,6 +4,7 @@ cd /backend/backend
 /opt/conda/envs/HSSE/bin/python manage.py migrate
 /opt/conda/envs/HSSE/bin/python manage.py collectstatic --noinput
 
-exec /opt/conda/envs/HSSE/bin/gunicorn hsse_backend.wsgi:application \
+exec /opt/conda/envs/HSSE/bin/gunicorn backend.wsgi:application \
     --bind 0.0.0.0:8080 \
-    --workers 3
+    --workers 3 \
+    --log-level debug
